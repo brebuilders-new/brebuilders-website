fc92bc08bac73bfa768ae31616bd2cf63fcca2c4
brebuilders-new <brebuilders@gmail.com>
2026-04-02 14:42:17 -0700

    config: add cdn.jsdelivr.net to Next.js image remotePatterns

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'brebuilders.com',
         pathname: '/wp-content/uploads/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        pathname: '/gh/brebuilders-new/bre-assets/**',
+      },
     ],
     formats: ['image/webp'],
   },
